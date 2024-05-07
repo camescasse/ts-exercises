e482cd2cb0f1a5ec8a43e26c98767cca83d2f3c8
camescasse <e.camescasse@gmail.com>
2024-05-07 17:58:01 -0400

    Add stopwatch exercise

---
+++ src/Basics/Stopwatch.ts
@@ -0,0 +1,41 @@
+class Stopwatch {
+  isRunning: boolean;
+  startTime: number;
+  endTime: number;
+  duration: number;
+
+  start() {
+    if (this.isRunning) {
+      console.log('Stopwatch is already running.');
+    } else {
+      this.isRunning = true;
+      this.startTime = Date.now();
+      console.log('Stopwatch is now running.');
+    }
+  }
+
+  stop() {
+    if (this.isRunning) {
+      this.isRunning = false;
+      this.endTime = Date.now();
+      console.log('Stopwatch is now stopped.');
+    } else {
+      console.log('Stopwatch is already stopped.');
+    }
+  }
+
+  getDuration() {
+    if (this.isRunning) {
+      this.endTime = Date.now();
+      const duration = this.endTime - this.startTime;
+      console.log(`Current duration is: ${duration}`);
+    } else if (!this.isRunning && this.endTime) {
+      const duration = this.endTime - this.startTime;
+      console.log(`Current duration is: ${duration}`);
+    } else {
+      console.log('Stopwatch is not running.');
+    }
+  }
+}
+
+export default Stopwatch;
